ad66c59d394e8ac24cd572fc9aeac0c01883f9d4
Wenbo Zhang <90272076+Falanan@users.noreply.github.com>
2023-04-15 02:17:57 -0400

    working on products page

--- public/scripts/products.js
+++ public/scripts/products.js
@@ -1,9 +1,90 @@
+$(document).ready(function(){
+    fetchData()
+})
+
+
+
+
+
 function fetchData() {
     fetch('https://dummyjson.com/products')
     .then(res => res.json())
     .then((data) => {
-        console.log(data);
+        // console.log(data.products[0]);
+        fillData(data.products);
     });
 }
 
-fetchData();
+
+
+function fillData(products) {
+    console.log("Products: ", products);
+
+    const container = document.getElementById("rowholder1");
+
+    for (let index = 0; index < products.length; index++) {
+        const product = products[index];
+
+        let rowHolder = document.createElement("div");
+        rowHolder.classList.add('column','is-4');
+
+        const card = document.createElement("div");
+        card.classList.add('card');
+
+        const cardImage = document.createElement('div');
+        cardImage.classList.add('card-image');
+
+        const figure = document.createElement('figure');
+        figure.classList.add('image', 'is-4by3');
+
+        const image = document.createElement('img');
+        image.src = product.images[0];
+        image.alt =  product.title + 'image';
+        image.width = 1280;
+        image.height = 960;
+
+        figure.appendChild(image);
+        cardImage.appendChild(figure);
+        card.appendChild(cardImage);
+
+        // Create the card content element
+        const cardContent = document.createElement('div');
+        cardContent.classList.add('card-content');
+
+        const media = document.createElement('div');
+        media.classList.add('media');
+
+        const mediaContent = document.createElement('div');
+        mediaContent.classList.add('media-content');
+
+        const productName = document.createElement('p');
+        productName.classList.add('title', 'is-4');
+        productName.textContent = product.title;
+
+        mediaContent.appendChild(productName);
+        media.appendChild(mediaContent);
+        cardContent.appendChild(media);
+
+        const content = document.createElement('div');
+        content.classList.add('content');
+        content.textContent = product.description;
+
+        const price = document.createElement('p');
+        price.classList.add('subtitle', 'is-6');
+        price.textContent = `Price: $${product.price.toFixed(2)}`;
+
+        content.appendChild(document.createElement('br'));
+        content.appendChild(document.createElement('br'));
+        content.appendChild(price);
+
+        cardContent.appendChild(content);
+        card.appendChild(cardContent);
+
+        // Append the card to the container
+        rowHolder.appendChild(card);
+
+        container.appendChild(rowHolder);
+        
+    }
+}
+
